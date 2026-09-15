25be37a9304a6bac2296b82e7aef39849b7507fa
Danil Mikhaylov <damikh13@gmail.com>
2026-09-16 00:43:23 +0300

    notes: 01. variables + iota, uncommented .go-file

--- 01_Introduction/02_variables/main.go
+++ 01_Introduction/02_variables/main.go
@@ -2,56 +2,35 @@ package main
 
 import "fmt"
 
-/*
-default values!
-:= <- available only inside functions
-to know a type: Printf("%T") +
-a, b = 10, 2.0
-int(2.6) -> 2
-const's
-const pi = 3.14 <- without := and int!
-const full_name = first_name + " " + last_name <- вычислится во время компиялции!
-
-bool
-string
-
-int	<- 32/64 => 1 bit for '+', '-', remaning for num
-uint <- 32/64 => 2
-
-int8, ..., int64
-uint8, ..., uint64
-
-byte = uint8
-rune = int32
-
-float32
-float64 <- default
-
-complex64, complex128
-*/
-
 func main() {
-	// username := "some_username"
-	// password := "1201202101"
+	username := "some_username"
+	password := "1201202101"
 
-	// var another_variable string
-	// var third_variable int
-	//
-	// another_variable = "15"
-	// third_variable = 12
+	var another_variable string
+	var third_variable int
 
-	// fmt.println("authorization: basic", username+":"+password)
-	// fmt.println(another_variable)
-	// fmt.println(third_variable)
+	another_variable = "15"
+	third_variable = 12
 
-	// var age int16
-	// var name string
-	// var cond bool
-	// fmt.Println(age, name, cond)
+	fmt.Println("authorization: basic", username+":"+password)
+	fmt.Println(another_variable)
+	fmt.Println(third_variable)
 
-	// age_ptr := &cond
-	// fmt.Printf("type(%d) = %T", age_ptr, age_ptr)
+	var age int16
+	var name string
+	var cond bool
+	fmt.Println(age, name, cond)
 
-	// f := 12.34
-	// fmt.Printf("type(%v) = %T\n", f, f)
+	age_ptr := &cond
+	fmt.Printf("type(%d) = %T", age_ptr, age_ptr)
+
+	f := 12.34
+	fmt.Printf("type(%v) = %T\n", f, f)
+
+	const (
+		Monday = iota // 0
+		Tuesday
+		Wednesday
+	)
+	fmt.Println(Wednesday)
 }
